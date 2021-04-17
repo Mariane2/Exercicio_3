481aad9b59a6c05d16ad80798989949ce9a45840
mariane <>
2021-04-17 19:51:11 +0000

    Inclusão de comentários

--- main.cpp
+++ main.cpp
@@ -20,25 +20,35 @@ char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
                       {'W' ,'X' ,'Y', 'Z'}};
 
   // Leitura da quantidade de letras da palavra:                    
-  std::cout << "Entre com a quantidade de letras da palavra:" 
-  << std:: endl;
+  std::cout << "Entre com a quantidade de letras da palavra:\n"; 
   std::cin >> N;
-  
+  std::cout<<"\n";
+
   // Entrada de dados no modelo #2=1
-  std::cout << "Entre com os dados: '#' + 'numero da tecla' + '=' + 'numero de vezes'. Exemplo: #2=1"
-  << std:: endl;
+  std::cout << "Entre com os dados: \n '#' + 'numero da tecla' + '=' + 'numero de vezes'. \n (Exemplo: #2=1) \n";
+
   
   // Armazena os dados de acordo com a qtd de palavras
   for (int cont=0;cont<N;cont++){ 
-    
+      
    std:: cin >> hashtag;
    std:: cin >> n_teclas[cont];
    std:: cin >> equal;
    std:: cin >> n_vezes[cont];
   
-  if((hashtag!= '#')||(n_teclas[cont] < 2)||(n_teclas[cont] > 9)||(equal!= '=')){
-   std::cout << "Algum(ns) dados estão incorretos, modelo aceito: 2#=2";
+  while((hashtag!= '#')||(n_teclas[cont] < 2)||(n_teclas[cont] > 9)||(equal!= '=')){
+
+   std::cout << "\n Algum(ns) dados estão incorretos, modelo aceito: 2#=2.  Entre com os dados novamente:\n";
+   
+   std:: cin >> hashtag;
+   std:: cin >> n_teclas[cont];
+   std:: cin >> equal;
+   std:: cin >> n_vezes[cont];
+
   }
+  
+   std::cout<<"\n";
+
 
   }
   
