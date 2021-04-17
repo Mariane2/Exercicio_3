c6bc29c6df8538d0b879b249e71cf19aec314683
mariane <>
2021-04-17 21:29:10 +0000

    Validação dos dados, linha 43

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 
+// ATIVIDADE 3 - Problema da escrita no celular (ao contrário)
+// Autor(a): Mariane Lima Dutra
+
 int main() {
 
 // Declaração das variáveis
 
-int N;              // Quantidade de letras da palavra
-int n_teclas[50];   // Número da tecla referente a letra desejada
-char hashtag;       // Armazena o caracter #
-char equal;         // Armazena o caracter =
-int n_vezes[50];    // Número de vezes que a tecla foi apertada.
+ int N;              // Quantidade de letras da palavra
+ int n_teclas[50];   // Número da tecla referente a letra desejada
+ char hashtag;       // Armazena o caracter #
+ char equal;         // Armazena o caracter =
+ int n_vezes[50];    // Número de vezes que a tecla foi apertada.
 
-char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
+
+  char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
                       {'D' ,'E' ,'F'},
                       {'G' ,'H' ,'I'},
                       {'J' ,'K' ,'L'},
-                      {'M' ,'N' ,'O'},
+                      {'M' ,'N' ,'O'}, 
                       {'P' ,'Q' ,'R','S'},
                       {'T' ,'U' ,'V'},
                       {'W' ,'X' ,'Y', 'Z'}};
@@ -25,10 +29,9 @@ char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
   std::cout<<"\n";
 
   // Entrada de dados no modelo #2=1
-  std::cout << "Entre com os dados: \n '#' + 'numero da tecla' + '=' + 'numero de vezes'. \n (Exemplo: #2=1) \n";
+  std::cout << "Entre com os dados: '#' + 'numero da tecla' + '=' + 'numero de vezes'. \n (Exemplo: #2=1) \n";
 
-  
-  // Armazena os dados de acordo com a qtd de palavras
+  // Armazena os dados de acordo com a qtd de palavras.
   for (int cont=0;cont<N;cont++){ 
       
    std:: cin >> hashtag;
@@ -36,7 +39,8 @@ char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
    std:: cin >> equal;
    std:: cin >> n_vezes[cont];
   
-  while((hashtag!= '#')||(n_teclas[cont] < 2)||(n_teclas[cont] > 9)||(equal!= '=')){
+  // Enquanto os dados não estiverem no formato correto,pede-se para preecher as informações novamente:
+  while((hashtag != '#')||(n_teclas[cont] < 2)||(equal != '=')||(n_vezes[cont] < 1)||(n_vezes[cont] > 4)){
 
    std::cout << "\n Algum(ns) dados estão incorretos, modelo aceito: 2#=2.  Entre com os dados novamente:\n";
    
@@ -45,19 +49,20 @@ char abcdario[8][4]= {{'A' ,'B', 'C'} ,      // Matriz abcdário
    std:: cin >> equal;
    std:: cin >> n_vezes[cont];
 
-  }
-  
-   std::cout<<"\n";
-
+   }
 
   }
   
-  for(int j=0;j<N;j++){
+  std::cout << "\n A palavra digitada é: \n";
 
-    std:: cout << abcdario[n_teclas[j]-2][n_vezes[j]-1];
-         
-}    
+  // Lê os vetores do numero da tecla e quantidade de vezes apertada e informa a sequência de caracteres correspondentes.
+  for(int j=0;j<N;j++){ 
+
+    std:: cout << abcdario[n_teclas[j]-2][n_vezes[j]-1];         
+  }    
    
-    return 0;
+    std::cout << "\n Aperte CNTRL + ENTER para reiniciar...";
+    
+  return 0;
 
 }
